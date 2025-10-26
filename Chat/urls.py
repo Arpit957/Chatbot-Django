00a58931e6960d39d77a7092feaa4ae2c83d65fa
Arpit957 <arpittiwari2001@gmail.com>
2025-10-26 17:48:51 +0530

    Final touch

--- Chat/urls.py
+++ Chat/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('checkview', views.checkview, name='checkview'),
     path('<str:room>/', views.room, name='room'),
     path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
 ]
     
